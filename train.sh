f044f43a7eb2b9bae602a45134df3d8b8fe9a5df
Bao Huynh <bao.huynh@mesoneer.io>
2025-03-10 20:57:06 +0700

    improve train

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 rm -rf dataset
 sh download-dataset.sh
-python runner/generate-metadata.py
-python melo/preprocess_text.py --metadata dataset/metadata.list
+python3 runner/generate-metadata.py
+pip install -r requirements.txt
+pip install -r OpenVoice/requirements.txt
+python3 melo/preprocess_text.py --metadata dataset/metadata.list
 bash melo/train.sh /melo/configs/config.json 2
